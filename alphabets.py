05c8ecc5f6d79f2c78d101b3ab4365d6aec8c3b4
karthik0100 <karthiksachin123@gmail.com>
2019-09-30 09:42:21 +0530

    To print Upper and Lower case alphabets

---
+++ alphabets.py
@@ -0,0 +1,19 @@
+def uppercasealphabets():
+    for c in range(65,91):
+        print(chr(c),end =",");
+
+def lowercasealphabets():
+    for c in range(97,123):
+                print(chr(c),end =",");
+
+
+print("\n");
+print("upper case alphabets","\n");
+uppercasealphabets();
+
+
+print("\n");
+print("lower case alphabets","\n");
+lowercasealphabets();
+
+
